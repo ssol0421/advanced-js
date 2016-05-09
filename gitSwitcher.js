5ff6036e01447c066aa7caee68e4a8169d9885bb
kdasol <Sol@Dasols-MacBook-Air.local>
2016-05-08 20:22:10 -0700

    pusing switch code

---
+++ gitSwitcher.js
@@ -0,0 +1,24 @@
+var userDay = prompt("What day is today?");
+Switch(useDay){
+		case "Monday":
+			console.log("Get enough coffee and survive");
+			break;
+		case "Tuesday":
+			console.log("Keep calm and have more coffee");
+			break;
+		case "Wednesday":
+			console.log("It's hump day");
+			break;
+		case "Thursday":
+			console.log("WooHoo! It's Happy Thursday");
+			break;
+		case "Friday":
+			console.log("You made it. It's Friday!");
+			break;
+		case "Saturday", "Sunday":
+			console.log("It's weekend. Let's take a rest");
+			break;
+		default:
+			console.log("Please give me a valid day^^");
+			break;
+}
